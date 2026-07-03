709c7a8e23b551ec59b103f0ec24c1add3235a3d
Fabio Moggi <fabio.moggi@gmail.com>
2026-07-03 16:13:28 -0300

    chore: updated index 3

--- functions/src/index.spec.ts
+++ functions/src/index.spec.ts
@@ -2,6 +2,6 @@ import { testHandler } from "./index.js";
 
 describe("testHandler", () => {
   it("returns expected message", () => {
-    expect(testHandler()).toEqual({ message: "Test from index 2" });
+    expect(testHandler()).toEqual({ message: "Test from index 3" });
   });
 });
