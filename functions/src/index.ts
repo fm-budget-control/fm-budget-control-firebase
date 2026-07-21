a1c8a25b7778c0c6bc791bd8c8855d1499e147f5
Fabio Moggi <fabio.moggi@gmail.com>
2026-07-21 15:33:12 -0300

    chore: address codex issues

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,1 @@
-import { onCall } from "firebase-functions/v2/https";
-import { withRole } from "./kernel/adapters/inbound/with-role.js";
-
 export { registerUser } from "./user/adapters/inbound/register-user.function.js";
-
-export function testHandler() {
-  return { message: "Test from index 3" };
-}
-
-export const test = onCall(withRole("user", testHandler));
